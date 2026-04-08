b2413272db53ce373a35e8f574892b090c5eb198
mathueseses12 <matheus.oliveira.rocha12@escola.pr.gov.br>
2026-04-08 11:04:16 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,29 @@
+// 1. exiba a mensagem 'ola mundo' no console
+(console.log('ola mundo'));
+
+// 2. crie uma variavel com seu nome e exiba o valor no console
+
+let nome = 'matesui';
+console.log(nome);
+
+// 3. some dois numero e exiba o resultado
+let soma = 5+2
+console.log(soma)
+
+// 4. subtraia dois numeros e exiba o resultado
+let sub = 10000000000-20
+console.log(sub)
+
+// 5. multiplique dois numeros e exiba o resultado
+let motipliquamento = 99999999999999999999999999999999999999999999999999999 * 999999999999999999999999999999999
+console.log(motipliquamento)
+
+// 6. descubra o resto da divisao de um numero para o outo
+let rsd = 11%2
+console.log(rsd)
+
+// 7. crie uma variavel booleana e exiba o seu valor (true e false)
+let eumorri = true;
+console.log(eumorri)
+
+// 8. verifique se um numero e maior que o outro
